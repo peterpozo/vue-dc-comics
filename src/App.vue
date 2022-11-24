8731397bf0efda16c9be583437cc6ad3e8fbe012
peterpozo <peteranderson.pozo@gmail.com>
2022-11-24 17:18:41 +0100

    Primo commit e header inizio

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <HeaderPage />
+    <MainPage />
+    <FooterPage />
+  </div>
+</template>
+
+<script>
+import HeaderPage from "@/components/HeaderPage.vue";
+import MainPage from "@/components/MainPage.vue";
+import FooterPage from "@/components/FooterPage.vue";
+export default {
+  components: {
+    HeaderPage,
+    MainPage,
+    FooterPage,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <img class="logo" src="../img/dc-logo.png" alt="logo" />
+    <nav class="links">
+      <li><a href="#!">CHARACTERS</a></li>
+      <li><a href="#!">COMICS</a></li>
+      <li><a href="#!">MOVIES</a></li>
+      <li><a href="#!">TV</a></li>
+      <li><a href="#!">GAMES</a></li>
+      <li><a href="#!">COLLECTIBLES</a></li>
+      <li><a href="#!">VIDEOS</a></li>
+      <li><a href="#!">FANS</a></li>
+      <li><a href="#!">NEWS</a></li>
+      <li><a href="#!">SHOP</a></li>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
