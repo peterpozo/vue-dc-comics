13b5dd36fbe0ed691690344df8a8060a840bc502
peterpozo <peteranderson.pozo@gmail.com>
2022-11-26 09:15:45 +0100

    creazione jumbotron

---
+++ src/components/HeaderJumbotron.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="jumbotron"></section>
+</template>
+
+<script>
+export default {
+  name: "HeaderJumbotron",
+  props: {
+    jumbotron: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.jumbotron {
+  padding: 15%;
+  background-image: url("../img/jumbotron.jpg");
+  background-size: cover;
+}
+</style>
