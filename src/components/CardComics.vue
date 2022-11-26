d62a295077c99424bc5e3dac73ee57f43dc55386
peterpozo <peteranderson.pozo@gmail.com>
2022-11-26 10:24:29 +0100

    creazione card e aggiunta nel main + style scss

---
+++ src/components/CardComics.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <img
+      src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+      alt=""
+    />
+    <h5>Action Comics</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardComics",
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  flex: 0 0 calc(100% / 6);
+  color: white;
+
+  img {
+    width: 90%;
+    aspect-ratio: 1 / 1;
+    object-fit: cover;
+    object-position: 2px 5px;
+  }
+}
+</style>
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,16 +1,30 @@
 <template>
   <main>
-    <div class="content"></div>
+    <div class="container">
+      <CardComics v-for="i in 12" :key="i" />
+    </div>
   </main>
 </template>
 
 <script>
-export default {};
+import CardComics from "@/components/CardComics.vue";
+export default {
+  components: {
+    CardComics,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.content {
-  padding: 80px;
+main {
+  padding: 4rem 0;
   background-color: #1c1c1c;
+}
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 auto;
+  padding: 0 1rem;
+  max-width: 1000px;
 }
 </style>
